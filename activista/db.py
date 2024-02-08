07a61d5d583dcc7f79d92b0b06a27e63fa2d53ed
McMmie <tharausamuel01@gmail.com>
2024-02-08 11:51:55 +0300

    updated the database

--- activista/db.py
+++ activista/db.py
@@ -29,3 +29,20 @@ def close_db(e=None):
 
     if db is not None:
         db.close()
+
+def init_db ():
+    """
+    a function that runs the sql commands in the schema file
+    """
+
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf-8'))
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """ Clear any existing data and create new tables. """
+    init_db()
+    click.echo('Initializing the database')
