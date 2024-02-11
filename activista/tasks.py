740f70a7823c140069e43d4cb0cb9f38c4fcbaf1
McMmie <tharausamuel01@gmail.com>
2024-02-11 15:40:12 +0300

    minor file changes

--- activista/tasks.py
+++ activista/tasks.py
@@ -2,6 +2,33 @@
 """
 adds a task to the database
 """
+import functools
 
-app.route('/new')
+
+@app.route('/new', methods=['GET', 'POST'])
+@login_required
+def create():
+    """
+    creats a new task
+    """
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required'
+            if error is not None:
+                flash(error)
+            else:
+                db =get_db()
+                db.execute(
+                        'INSERT INTO tasks (title, body) VALUES (?, ?, ?)',
+                        (title, body)
+                )
+                db.commit()
+                return redirect(url_for('home'))
+
+        render_template('create.html')
 
