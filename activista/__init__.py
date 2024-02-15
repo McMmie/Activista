1e384b99f3da028a8e5831c3217ef6ff53bd4bfc
McMmie <tharausamuel01@gmail.com>
2024-02-15 03:21:00 +0300

    made changes in the app factory

--- activista/__init__.py
+++ activista/__init__.py
@@ -27,12 +27,20 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
-    # a simple page that says hello
+    """# a simple page that says hello
     @app.route('/hello')
     def hello():
-        return 'Hello, World!'
+        return 'Hello, World!'"""
 
     from . import db
     db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+
+    from . import home
+    app.register_blueprint(home.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
